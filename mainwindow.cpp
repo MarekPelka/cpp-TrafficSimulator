abf8123bc89305c0ae45f66744f25cc17c99d054
michakrzeminski <micha.krzeminski@gmail.com>
2017-03-27 23:19:05 +0200

    Menu Actions

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,9 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include "Position.h"
 #include "Enums.h"
-#include "Vehicle.h"
-#include <list>
 #include "CityController.h"
 #include "GraphicFab.h"
 #include <QGraphicsItem>
@@ -15,6 +12,9 @@ MainWindow::MainWindow(QWidget *parent) :
 	ui(new Ui::MainWindow) {
 	ui->setupUi(this);
 
+    startPos = Position(0, 0);
+    endPos = Position(0, 0);
+
     //dropdown menu
     createActions();
     createMenus();
@@ -22,37 +22,10 @@ MainWindow::MainWindow(QWidget *parent) :
     setWindowTitle(QStringLiteral("Symulator ruchu miejskiego"));
 
     //initialize controllers
-	CityController *cityC = CityController::getInstance();
 	VehicleController *vehC = VehicleController::getInstance();
 	vehC->setMainWindow(this);
 
 	sigUpdatePositions.connect(boost::bind(&VehicleController::updatePositions, vehC, _1));
-
-	//timer for vehicle movement
-	timerId = startTimer(1000 / FPS);
-
-	//TESTING FOR CITI TOPOLOGY
-	Position p1(100, 100);
-	Position p2(500, 100);
-	Position p3(500, 500);
-	Position p4(100, 500);
-
-	cityC->addStreet(p1, p2);
-	cityC->addStreet(p2, p3);
-	cityC->addStreet(p3, p4);
-	cityC->addStreet(p4, p1);
-
-	cityC->addStreet(p2, p1);
-	cityC->addStreet(p3, p2);
-	cityC->addStreet(p4, p3);
-	cityC->addStreet(p1, p4);
-	//Crossing
-	Position pc3(300, 50);
-	Position pc4(300, 550);
-	cityC->addStreet(pc3, pc4);
-	Position pc1(50, 300);
-	Position pc2(550, 300);
-	cityC->addStreet(pc1, pc2);
 
 	//Painting streets
 	//TODO: There is some weird auto-scaling/positioning -> understend and fix
@@ -73,24 +46,8 @@ MainWindow::MainWindow(QWidget *parent) :
     infoLabel->setAlignment(Qt::AlignBottom);
     infoLabel->setFrameStyle(QFrame::StyledPanel | QFrame::Sunken);
     infoLabel->setStyleSheet("QLabel { color : white; }");
-    infoLabel->setGeometry(QRect(20, 20, 120, 20));
+    infoLabel->setGeometry(QRect(20, 20, 80, 20));
     scene->addWidget(infoLabel);
-
-	paintStreets();
-	paintIntersections();
-
-	//TESTING VEHICLES
-	Node n0(p4);
-	Node n1(p1);
-	Node n2(p2);
-	Node n3(p3);
-	std::list<Node> nodes = {n0,n1,n2,n3,n0,n1,n2,n3,n0,n1,n2,n3,n0,n1,n2,n3,n0,n1,n2,n3};
-	Vehicle car(CAR, nodes);
-	nodes.pop_front();
-	Vehicle truck(TRUCK, nodes);
-
-	vehC->addVehicle(car);
-	vehC->addVehicle(truck);
 }
 
 MainWindow::~MainWindow() {
@@ -136,11 +93,77 @@ void MainWindow::paintIntersections() {
 	}
 }
 
+std::list<Node> MainWindow::nodesPath(Position start, Position end)
+{
+    //TODO function that from start-end returns list of Nodes
+    //chceck which Node is closest to the startPos
+    //check which Node is closest to the endPos
+    //djikstra for path
+    std::list<Node> nodes;
+
+    //TESTING VEHICLES
+    Node n0(Position(100, 500));
+    Node n1(Position(100, 100));
+    Node n2(Position(500, 100));
+    Node n3(Position(500, 500));
+    nodes = { n0,n1,n2,n3,n0,n1,n2,n3,n0,n1,n2,n3,n0,n1,n2,n3,n0,n1,n2,n3 };
+    return nodes;
+}
+
 void MainWindow::timerEvent(QTimerEvent *event) {
 	sigUpdatePositions(int(1000 / FPS));
 }
+void MainWindow::mousePressEvent(QMouseEvent *event) {
+    QString status = infoLabel->text();
+    VehicleController *vehC = VehicleController::getInstance();
+    std::list<Node> nodes;
+    if (status.size() != 0) {
+        QPoint point(event->pos());
+        Position position(point.rx(), point.ry());
+        if(status == "Ulica") {
+
+        }
+        else if (status == "Budynek") {
+
+        }
+        else if (status == "Parking") {
+
+        }
+        else if (status == "Kamera") {
+
+        }
+        else if (status == QStringLiteral("Samochód") && click == false) {
+            click = true;
+            startPos = position;
+        }
+        else if (status == QStringLiteral("Samochód") && click == true) {
+            endPos = position;
+            nodes = nodesPath(startPos, endPos);
+            Vehicle car(CAR, nodes);
+            vehC->addVehicle(car);
+            click = false;
+        }
+        else if (status == QStringLiteral("Ciê¿arówka") && click == false) {
+            click = true;
+            startPos = position;
+        }
+        else if (status == QStringLiteral("Ciê¿arówka") && click == true) {
+            endPos = position;
+            nodes = nodesPath(startPos, endPos);
+            Vehicle truck(TRUCK, nodes);
+            vehC->addVehicle(truck);
+            click = false;
+        }
+    }
+}
+void MainWindow::mouseReleaseEvent(QMouseEvent *event) {
+    int i = 0;
+}
 
 void MainWindow::createActions() {
+    startAct = new QAction(tr("&Start"), this);
+    connect(startAct, &QAction::triggered, this, &MainWindow::start);
+
     scenario1Act = new QAction(tr("&Scenariusz 1"), this);
     connect(scenario1Act, &QAction::triggered, this, &MainWindow::scenario1);
 
@@ -149,37 +172,31 @@ void MainWindow::createActions() {
 
     exitAct = new QAction(tr("&Koniec"), this);
     exitAct->setShortcuts(QKeySequence::Close);
-    exitAct->setStatusTip(QStringLiteral("Zamknij aplikacjê"));
     connect(exitAct, &QAction::triggered, this, &QWidget::close);
 
     addStreetAct = new QAction(tr("&Ulica"), this);
     addStreetAct->setShortcut(QKeySequence(Qt::CTRL + Qt::Key_S));
-    addStreetAct->setStatusTip(QStringLiteral("Dodaj ulicê"));
+    addStreetAct->setIconVisibleInMenu(true);
     connect(addStreetAct, &QAction::triggered, this, &MainWindow::addStreet);
 
     addParkingAct = new QAction(tr("&Parking"), this);
     addParkingAct->setShortcut(QKeySequence(Qt::CTRL + Qt::Key_P));
-    addParkingAct->setStatusTip(tr("Dodaj parking"));
     connect(addParkingAct, &QAction::triggered, this, &MainWindow::addParking);
 
     addCameraAct = new QAction(tr("&Kamera"), this);
     addCameraAct->setShortcut(QKeySequence(Qt::CTRL + Qt::Key_C));
-    addCameraAct->setStatusTip(QStringLiteral("Dodaj kamerê"));
     connect(addCameraAct, &QAction::triggered, this, &MainWindow::addCamera);
 
     addBuildingAct = new QAction(tr("&Budynek"), this);
     addBuildingAct->setShortcut(QKeySequence(Qt::CTRL + Qt::Key_B));
-    addBuildingAct->setStatusTip(tr("Dodaj budynek"));
     connect(addBuildingAct, &QAction::triggered, this, &MainWindow::addBuilding);
 
     addCarAct = new QAction(QStringLiteral("&Samochód"), this);
     addCarAct->setShortcut(QKeySequence(Qt::SHIFT + Qt::Key_C));
-    addCarAct->setStatusTip(QStringLiteral("Dodaj samochód"));
     connect(addCarAct, &QAction::triggered, this, &MainWindow::addCar);
 
     addTruckAct = new QAction(QStringLiteral("&Ciê¿arówka"), this);
     addTruckAct->setShortcut(QKeySequence(Qt::SHIFT + Qt::Key_T));
-    addTruckAct->setStatusTip(QStringLiteral("Dodaj ciê¿arówkê"));
     connect(addTruckAct, &QAction::triggered, this, &MainWindow::addTruck);
 
     aboutAct = new QAction(tr("&O Autorach"), this);
@@ -188,6 +205,7 @@ void MainWindow::createActions() {
 
 void MainWindow::createMenus() {
     fileMenu = menuBar()->addMenu(tr("&Plik"));
+    fileMenu->addAction(startAct);
     fileMenu->addAction(scenario1Act);
     fileMenu->addAction(scenario2Act);
     fileMenu->addSeparator();
@@ -215,29 +233,57 @@ void MainWindow::contextMenuEvent(QContextMenuEvent *event) {
     menu.exec(event->globalPos());
 }
 
+void MainWindow::start() {
+    //timer for vehicle movement
+    timerId = startTimer(1000 / FPS);
+}
 void MainWindow::scenario1() {
+    CityController *cityC = CityController::getInstance();
+    Position p1(100, 100);
+    Position p2(500, 100);
+    Position p3(500, 500);
+    Position p4(100, 500);
 
+    cityC->addStreet(p1, p2);
+    cityC->addStreet(p2, p3);
+    cityC->addStreet(p3, p4);
+    cityC->addStreet(p4, p1);
+
+    cityC->addStreet(p2, p1);
+    cityC->addStreet(p3, p2);
+    cityC->addStreet(p4, p3);
+    cityC->addStreet(p1, p4);
+    //Crossing
+    Position pc3(300, 50);
+    Position pc4(300, 550);
+    cityC->addStreet(pc3, pc4);
+    Position pc1(50, 300);
+    Position pc2(550, 300);
+    cityC->addStreet(pc1, pc2);
+
+    paintStreets();
+    paintIntersections();
 }
 void MainWindow::scenario2() {
 
 }
 void MainWindow::addStreet() {
-    infoLabel->setText(tr("Dodawanie ulicy"));
+    infoLabel->setText(tr("Ulica"));
 }
 void MainWindow::addParking() {
-    infoLabel->setText(tr("Dodawanie parkingu"));
+    infoLabel->setText(tr("Parking"));
 }
 void MainWindow::addCamera() {
-    infoLabel->setText(tr("Dodawanie kamery"));
+    infoLabel->setText(tr("Kamera"));
 }
 void MainWindow::addBuilding() {
-    infoLabel->setText(tr("Dodawanie budynku"));
+    infoLabel->setText(tr("Budynek"));
 }
 void MainWindow::addCar() {
-    infoLabel->setText(tr("Dodawanie samochodu"));
+    infoLabel->setText(QStringLiteral("Samochód"));
 }
 void MainWindow::addTruck() {
-    infoLabel->setText(QStringLiteral("Dodawanie ciê¿arówki"));
+    infoLabel->setText(QStringLiteral("Ciê¿arówka"));
 }
 void MainWindow::about() {
     QMessageBox::about(this, tr("O Autorach"),
