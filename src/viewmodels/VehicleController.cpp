d7506f7a7b55570bc56a0798d03a1e1dbe3b3c82
Marek Pelka <pelkam95@gmail.com>
2017-05-20 20:39:46 +0200

    Fix for multithreading

--- src/viewmodels/VehicleController.cpp
+++ src/viewmodels/VehicleController.cpp
@@ -10,24 +10,34 @@
 VehicleController* VehicleController::instance = nullptr;
 
 VehicleController* VehicleController::getInstance() {
-    if (!instance)
-        instance = new VehicleController;
-    return instance;
+	if (!instance)
+		instance = new VehicleController;
+	return instance;
 }
 
-VehicleController::VehicleController() {}
+VehicleController::VehicleController() {
+	vehiclesToSwitch = std::list<Vehicle>();
+}
+
+std::list<Vehicle> * VehicleController::getVehiclesToSwitch() {
+	return &vehiclesToSwitch;
+}
 
 void VehicleController::addVehicle(Vehicle vehicle) {
 	if (auto spt = vehicle.getNodes().front().getStreetsIn().at(vehicle.getOrientation()).lock()) { // Has to be copied into a shared_ptr before usage	
 		spt->addVehicleToStreet(vehicle);
 	}
-    //vehicles.push_back(vehicle);
+	//vehicles.push_back(vehicle);
+}
+
+void VehicleController::addVehicleToSwitch(Vehicle vehicle) {
+	vehiclesToSwitch.push_back(vehicle);
 }
 
 void VehicleController::addPedestrian(Pedestrian ped) {
-    if (auto spt = ped.getNodes().front().getStreetsIn().at(ped.getOrientation()).lock()) { // Has to be copied into a shared_ptr before usage	
-        spt->addPedestrianToStreet(ped);
-    }
+	if (auto spt = ped.getNodes().front().getStreetsIn().at(ped.getOrientation()).lock()) { // Has to be copied into a shared_ptr before usage	
+		spt->addPedestrianToStreet(ped);
+	}
 }
 
 std::list<Vehicle> VehicleController::getVehicles() {
@@ -40,12 +50,12 @@ std::list<Vehicle> VehicleController::getVehicles() {
 }
 
 std::list<Pedestrian> VehicleController::getPedestrians() {
-    std::list<Pedestrian> out;
-    for (PStreet s : CityController::getInstance()->getStreets()) {
-        std::vector<Pedestrian> * temp = s->getPedestrians();
-        out.insert(out.end(), temp->begin(), temp->end());
-    }
-    return out;
+	std::list<Pedestrian> out;
+	for (PStreet s : CityController::getInstance()->getStreets()) {
+		std::vector<Pedestrian> * temp = s->getPedestrians();
+		out.insert(out.end(), temp->begin(), temp->end());
+	}
+	return out;
 }
 
 void VehicleController::updatePositions(int interval) {
@@ -61,8 +71,17 @@ void VehicleController::updatePositions(int interval) {
 	}
 	for (int i = 0; i < threadsVC.size(); i++) {
 		//if (threadsVC.at(i).joinable())
-			threadsVC.at(i).join();
+		threadsVC.at(i).join();
 	}
+	for (Vehicle v : vehiclesToSwitch) {
+		v.getStreetToSwitch()->addVehicleToStreet(v);
+	}
+	//getVehiclesToSwitch().clear();
+	auto deleteIterator = vehiclesToSwitch.begin();
+	while (deleteIterator != vehiclesToSwitch.end()) {
+		deleteIterator = vehiclesToSwitch.erase(deleteIterator);
+	}
+	//s->addVehicleToStreet(vec.front());
 	//threadsVC.back().join();
 
 }
@@ -70,7 +89,6 @@ void VehicleController::updatePositions(int interval) {
 void VehicleController::updatePositionCallback(PStreet s, int arg) {
 	s->updatePositions(arg);
 	//args.first->updatePositions(args.second);
-	//std::cout << "HWDP";
 }
 
 void VehicleController::clearController() {
--- src/viewmodels/VehicleController.h
+++ src/viewmodels/VehicleController.h
@@ -4,22 +4,29 @@
 #include "../models/Vehicle.h"
 #include <list>
 #include <memory>
-#include "../models/Street.h"
+//#include "../models/Street.h"
 #include "CityController.h"
+
+class Street;
+typedef std::shared_ptr<Street> PStreet;
 
 class VehicleController {
 public:
-    void addVehicle(Vehicle vehicle);
-    void addPedestrian(Pedestrian ped);
-    std::list<Vehicle> getVehicles();
-    std::list<Pedestrian> getPedestrians();
-    void updatePositions(int interval);
-    void clearController();
+	void addVehicle(Vehicle vehicle);
+	void addVehicleToSwitch(Vehicle vehicle);
+	void addPedestrian(Pedestrian ped);
+	std::list<Vehicle> getVehicles();
+	std::list<Pedestrian> getPedestrians();
+	std::list<Vehicle> * getVehiclesToSwitch();
+	void updatePositions(int interval);
+	void clearController();
 
-    static VehicleController* getInstance();
+	static VehicleController* getInstance();
 private:
-    VehicleController();
-    static VehicleController* instance;
+	VehicleController();
+
+	static VehicleController* instance;
 	void updatePositionCallback(PStreet s, int arg);
+	std::list<Vehicle> vehiclesToSwitch;
 };
 #endif
