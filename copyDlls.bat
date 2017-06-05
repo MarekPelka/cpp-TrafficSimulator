ea400d57ec6e061412d5e05367ec4a1c7486be8f
michakrzeminski <micha.krzeminski@gmail.com>
2017-06-05 16:15:10 +0200

    bat file for copying dlls on windows

---
+++ copyDlls.bat
@@ -0,0 +1,4 @@
+xcopy C:\Qt\5.8\msvc2015_64\bin\Qt5Gui.dll . /D /Y
+xcopy C:\Qt\5.8\msvc2015_64\bin\Qt5Core.dll . /D /Y
+xcopy C:\Qt\5.8\msvc2015_64\bin\Qt5Widgets.dll . /D /Y
+xcopy C:\Qt\5.8\msvc2015_64\bin\Qt5Sql.dll . /D /Y
